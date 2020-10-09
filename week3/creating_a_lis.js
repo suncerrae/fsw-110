912872814e99b6e2f3a821a28a708d7b7bfc71c9
suncerrae <sun.quinones@bryanuniverrsity.edu>
2020-10-08 21:39:07 -0400

    first commit

---
+++ week3/creating_a_lis.js
@@ -0,0 +1,25 @@
+for (let i = 0; i < 10; i++) {
+	saySomething();
+}
+
+function saySomething() {
+	document.writeln("<h1>Hello World!<h1>");
+}
+
+// const people = [
+//     {name: 'Steve', age: 23}, 
+//     {name: 'Larry', age: 3}, 
+//     {name: 'Joe', age: 8}, 
+//     {name: 'Shirley', age: 14}, 
+//     {name: 'Nate', age: 17}, 
+//     {name: 'Rick', age: 14}, 
+//     {name: 'Emily', age: 4}, 
+//     {name: 'Steph', age: 37}];
+
+// const anyAdult = people.some(person => person.age >= 18);
+
+
+// console.log(anyAdult); // true
+
+
+
